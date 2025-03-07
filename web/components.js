ca21a8f8f9c9e79b4ccb281a2694cabcfa6cf79c
Foxication <116384808+foxication@users.noreply.github.com>
2025-03-08 04:12:52 +0900

    Webview design

--- web/components.js
+++ web/components.js
@@ -38,3 +38,51 @@
     }
   }
 })();
+
+function dontIndent(str) {
+  return ('' + str).replace(/(\n)\s+/g, '$1');
+}
+
+customElements.define(
+  'ext-component',
+  class ExtComponent extends HTMLElement {
+    connectedCallback() {
+      const typePath = this.getAttribute('type-path');
+      this.outerHTML = dontIndent(`
+        <details class="is-expandable">
+          <summary class="component">
+            <svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="header-icon">
+              <path fill-rule="evenodd" clip-rule="evenodd" d="M10.072 8.024L5.715 3.667l.618-.62L11 7.716v.618L6.333 13l-.618-.619 4.357-4.357z">
+              </path>
+            </svg>
+            <span>${typePath}</span>
+          </summary>
+          <div class="details-content" type-path="${typePath}">${this.innerHTML}</div>
+        </details>`);
+    }
+  }
+);
+
+customElements.define(
+  'ext-declaration',
+  class ExtDeclaration extends HTMLElement {
+    connectedCallback() {
+      const parentProperty = this.parentElement?.getAttribute('property') ?? '';
+      const property = this.getAttribute('property');
+
+      if (property === null) {
+        this.outerHTML = dontIndent(`
+          <div class="row">
+            <vscode-textfield id="${parentProperty}"/>
+          </div>`);
+      } else {
+        const id = parentProperty + '.' + property;
+        this.outerHTML = dontIndent(`
+          <div class="row">
+            <label for="${id}" class="property">${property}</label>
+            <vscode-textfield id="${id}"/>
+          </div>`);
+      }
+    }
+  }
+);
