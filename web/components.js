32b5c114efa45a7c9b5e7d3ffddc17562f378315
Foxication <116384808+foxication@users.noreply.github.com>
2025-03-12 04:14:31 +0900

    Development
    - Remove accidental appearing of scrollbar in body
    - Fix color of text so it looks nice in all themes
    - Unset scrollbar color to have webkit scrollbar
    - Add component with long labels for testing
    - Implement textarea in ext-text
    - Refactor code in creation of html elements for stability
    - Synchronize value between elements of ext-text
    - Change icons for switching mode in ext-text

--- web/components.js
+++ web/components.js
@@ -110,7 +110,6 @@
       }
       div.value {
         flex: 5;
-        height: 26px;
 
         vscode-checkbox {
           width: 100%;
@@ -151,14 +150,36 @@
         outline: none;
         padding: 3px 4px;
         width: 100%;
-        
       }
         
       input.input:focus-visible {
         outline-offset: 0px;
       }
-        
-      input::placeholder {
+      
+      textarea {
+        background-color: var(--vscode-settings-textInputBackground, #313131);
+        box-sizing: border-box;
+        color: var(--vscode-settings-textInputForeground, #cccccc);
+        display: block;
+        font-family: var(--vscode-font-family, sans-serif);
+        font-size: var(--vscode-font-size, 13px);
+        font-weight: var(--vscode-font-weight, normal);
+        width: 100%;
+        line-height: 18px;
+        padding: 3px 4px;
+        padding-bottom: 9px;
+        text-wrap: nowrap;
+        resize: none;
+        border: 0px;
+      }
+      textarea:focus {
+        outline: none;
+      }
+      textarea::-webkit-scrollbar { 
+        display: none;
+      }
+      
+      ::placeholder {
         color: var(--vscode-input-placeholderForeground, #989898);
         opacity: 1;
       }
@@ -171,6 +192,7 @@
         justify-content: center;
         cursor: pointer;
         flex: none;
+        border-radius: inherit;
       }
       div:hover {
         background-color: var(--vscode-toolbar-hoverBackground,rgba(90, 93, 94, 0.31));
@@ -221,19 +243,33 @@
       connectedCallback() {
         const path = this.getAttribute('path');
         const parentPath = this.parentElement?.getAttribute('path');
-        const indentation = '<div class="space"></div>'.repeat((parentPath ?? '').replace(/[^.]/g, '').length);
+        // const indentation = '<div class="space"></div>'.repeat((parentPath ?? '').replace(/[^.]/g, '').length);
 
+        // Initialize elements
+        const indentation = document.createElement('div');
+        indentation.style.width = ((parentPath ?? '').replace(/[^.]/g, '').length * 22 - 6).toString() + 'px';
+        indentation.className = 'space';
+        const icon = document.createElement('vscode-icon');
+        icon.setAttribute('name', 'chevron-right');
+        icon.setAttribute('class', 'header-icon');
+        const label = document.createElement('span');
+        label.innerText = path ?? '';
+        const summary = document.createElement('summary');
+        summary.appendChild(indentation);
+        summary.appendChild(icon);
+        summary.appendChild(label);
+        const content = document.createElement('div');
+        content.setAttribute('class', 'details-content');
+        content.setAttribute('path', fullPath(parentPath, path));
+        content.innerHTML = this.innerHTML;
+        const details = document.createElement('details');
+        details.appendChild(summary);
+        details.appendChild(content);
+
+        // Create shadow DOM
         const shadow = this.attachShadow({ mode: 'open' });
         shadow.adoptedStyleSheets = [styleForExpandable];
-        shadow.innerHTML = dontIndent(`
-          <details>
-            <summary>
-              ${indentation}
-              <vscode-icon name="chevron-right" class="header-icon"></vscode-icon>
-              <span>${path ?? ''}</span>
-            </summary>
-            <div class="details-content" path="${fullPath(parentPath, path)}">${this.innerHTML}</div>
-          </details>`);
+        shadow.appendChild(details);
       }
     }
   );
@@ -246,26 +282,42 @@
         const full = fullPath(this.parentElement?.getAttribute('path'), path);
         const type = this.getAttribute('type');
 
-        const shadow = this.attachShadow({ mode: 'open' });
-        shadow.adoptedStyleSheets = [styleForDeclaration];
-        let declaration = dontIndent(`
-          <label for="${full}">${path ?? ''}</label>
-          <div class="value">`);
+        // Initialize elements
+        const label = document.createElement('label');
+        label.setAttribute('for', full);
+        label.innerText = path ?? '';
+
+        const valueHolder = document.createElement('div');
+        valueHolder.classList.add('value');
+
         switch (type) {
           case 'boolean':
-            declaration += `<vscode-checkbox id="${full}"/>`;
+            const checkbox = document.createElement('vscode-checkbox');
+            checkbox.id = full;
+            valueHolder.appendChild(checkbox);
             break;
 
           case 'enum':
-            declaration += `<vscode-single-select id="${full}">${this.innerHTML}</vscode-single-select>`;
+            const select = document.createElement('vscode-single-select');
+            select.id = full;
+            select.innerHTML = this.innerHTML;
+            valueHolder.appendChild(select);
             break;
 
           default: // string
-            declaration += `<ext-text id="${full}" />`;
+            const text = document.createElement('ext-text');
+            text.id = full;
+            if ('value' in text) text.value = 'Hahaha';
+            if (this.hasAttribute('experiment')) text.setAttribute('type', 'area');
+            valueHolder.appendChild(text); // TODO: somehow update value
             break;
         }
-        declaration += '</div>';
-        shadow.innerHTML = declaration;
+
+        // Create shadow DOM
+        const shadow = this.attachShadow({ mode: 'open' });
+        shadow.adoptedStyleSheets = [styleForDeclaration];
+        shadow.appendChild(label);
+        shadow.appendChild(valueHolder);
       }
     }
   );
@@ -274,59 +326,118 @@
     'ext-text',
     class ExtText extends HTMLElement {
       inEdit = false;
-      state = 'field';
+      value = '';
 
       connectedCallback() {
         const placeholder = this.getAttribute('placeholder');
         const isDisabled = this.hasAttribute('disabled');
 
-        // Initialize elements
+        // Initialize elements of input
         const field = document.createElement('input');
         field.setAttribute('type', 'text');
-        field.setAttribute('value', this.getAttribute('value') ?? '');
         field.setAttribute('placeholder', placeholder ?? '');
         if (isDisabled) field.setAttribute('disabled', '');
 
         const toArea = document.createElement('div');
         const iconArea = document.createElement('vscode-icon');
-        iconArea.setAttribute('name', 'list-flat');
+        iconArea.setAttribute('name', 'list-selection');
         toArea.appendChild(iconArea);
+
+        // Initialize elements of textarea
+        const area = document.createElement('textarea');
+        if (isDisabled) area.setAttribute('disabled', '');
+        area.setAttribute('rows', '5');
+
+        const toField = document.createElement('div');
+        toField.className = 'inArea';
+        const iconField = document.createElement('vscode-icon');
+        iconField.setAttribute('name', 'symbol-string');
+        toField.appendChild(iconField);
 
         // Initialize shadow DOM
         const shadow = this.attachShadow({ mode: 'open' });
         shadow.adoptedStyleSheets = [styleForTextInput];
-        shadow.appendChild(field);
-        if (!isDisabled) shadow.appendChild(toArea);
 
-        // Logics of field
-        field.onfocus = (e) => {
-          field.select();
+        shadow.appendChild(area);
+        shadow.appendChild(field);
+        if (!isDisabled) {
+          shadow.appendChild(toField);
+          shadow.appendChild(toArea);
+        }
+
+        // Switchers
+        toArea.onclick = (e) => {
+          area.value = this.value;
+
+          area.style.display = 'block';
+          toField.style.display = 'flex';
+          field.style.display = 'none';
+          toArea.style.display = 'none';
+        };
+        toField.onclick = (e) => {
+          field.value = this.value;
+
+          area.style.display = 'none';
+          toField.style.display = 'none';
+          field.style.display = 'block';
+          toArea.style.display = 'flex';
+        };
+
+        switch (this.getAttribute('type')) {
+          case 'area':
+            toArea.onclick(new MouseEvent(''));
+            break;
+
+          default:
+            toField.onclick(new MouseEvent(''));
+            break;
+        }
+
+        // Logics of area
+        area.oninput = (e) => {
+          area.style.height = 'auto';
+          area.style.height = area.scrollHeight + 'px';
+        };
+        area.onfocus = (e) => {
           this.setAttribute('focused', '');
           this.inEdit = true;
         };
-        field.onmouseup = (e) => {
-          if (!this.inEdit) {
-            e.preventDefault();
+        area.onchange = () => {
+          this.value = area.value;
+          if (this.inEdit) {
+            area.blur();
           }
+        };
+        area.onblur = () => {
+          area.value = this.value;
+          area.scrollTo(0, 0);
+          this.removeAttribute('focused');
+          this.inEdit = false;
+        };
+
+        // Logics of field
+        field.onfocus = (e) => {
+          this.setAttribute('focused', '');
+          this.inEdit = true;
         };
         field.onkeydown = (e) => {
           if (!('key' in e)) {
             return;
           }
           if (e.key === 'Escape' || e.key === 'Esc') {
-            field.value = this.getAttribute('value') ?? '';
+            field.value = this.value;
             field.blur();
             e.preventDefault();
           }
         };
         field.onchange = () => {
-          this.setAttribute('value', field.value);
+          this.value = field.value;
           if (this.inEdit) {
             field.blur();
           }
         };
         field.onblur = () => {
-          field.value = this.getAttribute('value') ?? '';
+          field.value = this.value;
           this.removeAttribute('focused');
           this.inEdit = false;
         };
