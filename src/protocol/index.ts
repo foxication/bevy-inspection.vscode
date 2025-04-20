d89822c79dcdb3ae6a7be22a236b1c7b5ae7cdf1
Foxication <116384808+foxication@users.noreply.github.com>
2025-04-21 01:24:58 +0900

    Remove useless function

--- src/protocol/index.ts
+++ src/protocol/index.ts
@@ -1,7 +1,2 @@
-import { TypePath } from './types';
-
 export * from './protocol';
 export * from './types';
-export function short(typePath: TypePath) {
-  return (/[^::]*$/.exec(typePath.split('<')[0]) ?? '???')[0];
-}
